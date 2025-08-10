bcd597e06c94ead99f5c6cff98e1c0f30a43f193
Jeferson Alvarez <mymotre213q@gmail.com>
2025-08-10 00:13:13 -0500

    Agregando el header

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,5 +1,20 @@
 <template>
-    <div>
-        <p>Hola mundo</p>
-    </div>
+  <div class="min-h-screen flex flex-col bg-gray-50">
+    <!-- Header -->
+    <HeaderView />
+  </div>
 </template>
+
+<script setup>
+// Pinia store can be imported here when needed
+// import { useMainStore } from '@/stores/main'
+// const store = useMainStore()
+import HeaderView from '@/components/HomeViewComponents/HeaderView.vue';
+</script>
+
+<style scoped>
+/* Custom styles */
+.prose p {
+  margin-bottom: 1rem;
+}
+</style>
