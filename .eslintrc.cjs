b409489ead6658ecbc47dfed59f45162635b0517
Jeferson Alvarez <mymotre213q@gmail.com>
2025-08-09 19:59:49 -0500

    Subiendo estructura del proyecto

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/vue3-essential",
+    "plugin:prettier/recommended"
+  ],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  rules: {
+    "vue/multi-word-component-names": "off",
+    "no-unused-vars": "warn"
+  }
+}
---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <div class="app">
+    <RouterView />
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+
+</script>
